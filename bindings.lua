f0fa983c379618fbd5fc65d1903a5824b5832f0a
Sergey Kostyaev <sergey.kostyaev@eltex.loc>
2016-04-06 14:03:17 +0600

    rebind fullscreen for compat with emacs

--- bindings.lua
+++ bindings.lua
@@ -102,7 +102,7 @@ globalkeys = awful.util.table.join(
 )
 
 clientkeys = awful.util.table.join(
-    awful.key({ modkey,           }, "f",      function (c) c.fullscreen = not c.fullscreen  end),
+    awful.key({ "Shift", "Control"}, "f",      function (c) c.fullscreen = not c.fullscreen  end),
     awful.key({ "Shift", "Control"}, "q",      function (c) c:kill()                         end),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
