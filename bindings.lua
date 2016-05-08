7631dda123385be3032234e35db26e9deee5f20b
Sergey Kostyaev <s.kostyaev@office.ngs.ru>
2016-05-08 19:58:18 +0600

    hide meny after click on empty desktop

--- bindings.lua
+++ bindings.lua
@@ -2,6 +2,7 @@ local awful = require("awful")
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
+    awful.button({ }, 1, function () mymainmenu:hide() end),
     awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
